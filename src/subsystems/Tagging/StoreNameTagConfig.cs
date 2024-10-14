f47879dbbcb07a05fd848c63bd5f14b498cf32fb
kuroma6666 <kuroma.creator@gmail.com>
2024-10-14 22:49:49 +0900

    feat: 店名と費用項目を設定ファイルに保持するための`StoreNameTagConfig`クラスを実装

---
+++ src/subsystems/Tagging/StoreNameTagConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nett;
+
+
+namespace SonyBankUsageRecordParse.src.subsystems.Tagging
+{
+	public class StoreNameTagConfig
+	{
+		private const String TomlFilePath = "storeNameTagConfig.toml";
+		public List<String> StoreNameTags { get; private set; } = [];
+
+		public StoreNameTagConfig()
+		{
+			Load();
+
+		}
+		public void Load()
+		{
+			if (System.IO.File.Exists(TomlFilePath))
+			{
+				var table = Toml.ReadFile(TomlFilePath);
+				if (table.TryGetValue("StoreNameTag", out TomlObject tags))
+				{
+					// 値がTomlArray型かを確認し、キャストする
+					if (tags is TomlArray tagsArray)
+					{
+						// TomlArrayをリストに変換
+						StoreNameTags = tagsArray.Items.Select(t => t.Get<string>()).ToList();
+					}
+				}
+			}
+		}
+
+		public void Write()
+		{
+			TomlTable table = Toml.Create();
+			table.Add("StoreNameTag", StoreNameTags.ToArray());
+			Toml.WriteFile(table, TomlFilePath);
+		}
+	}
+}
